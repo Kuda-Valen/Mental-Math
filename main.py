2787d9610234007a1c5026b9ed9618d58f0ec69a
Kuda_Valen <kudamukwasi@gmail.com>
2026-08-06 23:58:21 +0200

    template

---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+if __name__ == "__main__":
+
+    while True:
+        print("\n === MENTAL MATH ===")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Challenge")
+        print("6. Exit")
